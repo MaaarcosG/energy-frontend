a5e3d6e70d109ee94748ceae6e9022c89e6c0379
marcos.gutierrez@twowheelstogo.com <marcos.gutierrez@twowheelstogo.com>
2024-06-06 15:53:18 -0600

    update deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  distDir: "dist",
+  reactStrictMode: true,
   images: {
     domains: ["raw.githubusercontent.com"],
+    unoptimized: true,
   },
 };
 
--- src/app/graphic/[slug]/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const GraphicPage = () => {
-  return (
-    <div>page</div>
-  )
-}
-
-export default GraphicPage
